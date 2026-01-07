e7786877debff8ac8e95999f23d973a2961b18fd
Joy Saraswaty <joyprasanna.saraswaty@tcs.com>
2026-01-07 22:16:09 +0000

    Launchpad commit2

--- app/school-student-management/dist/test/integration/pages/SchoolObjectPage.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/fe/test/ObjectPage"],function(e){"use strict";var t={actions:{},assertions:{}};return new e({appId:"schoolstudentmanagement",componentId:"SchoolObjectPage",contextPath:"/School"},t)});
-//# sourceMappingURL=SchoolObjectPage.js.map
