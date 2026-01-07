6d384ffed07d8d48cd60bd34c663647bb42533f2
Joy Saraswaty <joyprasanna.saraswaty@tcs.com>
2026-01-07 02:31:15 +0000

    Remove build folder to generate from CI/CD build commit1

--- app/school-student-management/dist/test/integration/pages/SchoolList.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/fe/test/ListReport"],function(t){"use strict";var e={actions:{},assertions:{}};return new t({appId:"schoolstudentmanagement",componentId:"SchoolList",contextPath:"/School"},e)});
-//# sourceMappingURL=SchoolList.js.map
